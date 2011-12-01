ed3dae0559d8ba9f03b6bfad6d3e40fd0f85f1ff
Matthew Rudy Jacobs <MatthewRudyJacobs@gmail.com>
2011-12-01 15:33:29 +0800

    revise the generator

--- lib/generators/one_sky/init/init_generator.rb
+++ lib/generators/one_sky/init/init_generator.rb
@@ -2,16 +2,18 @@ module OneSky
   module Generators
     class InitGenerator < ::Rails::Generators::Base
       desc "This generator configures i18n-one_sky for use on this Rails project."
-      argument :api_key, :type => :string, :desc => "The API key you got from OneSky"
-      argument :api_secret, :type => :string, :desc => "The API secret you got from OneSky"
-      argument :project, :type => :string, :desc => "The name of the OneSky project"
-      argument :platform_id, :type => :integer, :desc => "The id of the platform you want to translate for"
-      class_option :force, :type => :boolean, :default => false, :desc => "Overwrite if config file already exists"
-      
-      @@config_file = File.join(Rails.root.to_s, 'config', 'one_sky.yml')
+
+      argument :api_key,     :type => :string, :desc => "The API key you got from OneSky"
+      argument :api_secret,  :type => :string, :desc => "The API secret you got from OneSky"
+      argument :project,     :type => :string, :desc => "The name of the OneSky project"
+      argument :platform_id, :type => :string, :desc => "The id of the OneSky platform"
+
+      class_option :force,   :type => :boolean, :default => false, :desc => "Overwrite if config file already exists"
+
+      CONFIG_PATH = File.join(Rails.root.to_s, 'config', 'one_sky.yml')
 
       def remove_config_file
-        if File.exists? @@config_file
+        if File.exists? CONFIG_PATH
           if options.force?
             say_status("warning", "config file already exists and is being overwritten.", :yellow)
             remove_file @@config_file
@@ -23,12 +25,12 @@ module OneSky
       end
 
       def config_hash
-        {"api_key" => api_key, "api_secret" => api_secret, "project" => project, "platform_id" => platform_id}
+        {"api_key" => api_key, "api_secret" => api_secret, "project" => project, "platform_id" => platform_id.to_i}
       end
-      
+
       def create_config_file
-        create_file(@@config_file, config_hash.to_yaml)
-        say_status("info", "config file #{@@config_file} created.", :green)
+        create_file(CONFIG_PATH, config_hash.to_yaml)
+        say_status("info", "config file #{CONFIG_PATH} created.", :green)
       end
     end
   end
