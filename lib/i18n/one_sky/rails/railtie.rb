54c77af3602b2a31f76d50edd7b86bf44362082d
Jonathan Nevelson <jnevelson@gmail.com>
2011-12-29 14:21:06 -0800

    Rails 2 compatibility

--- lib/i18n/one_sky/rails/railtie.rb
+++ lib/i18n/one_sky/rails/railtie.rb
@@ -1,8 +1,10 @@
 module I18n
   module Onesky
-    class Railtie < ::Rails::Railtie
-      rake_tasks do
-        load "i18n/one_sky/rails/tasks/i18n-one_sky.rake"
+    if Rails::VERSION::MAJOR >= 3
+      class Railtie < ::Rails::Railtie
+        rake_tasks do
+          load "i18n/one_sky/rails/tasks/i18n-one_sky.rake"
+        end
       end
     end
   end
