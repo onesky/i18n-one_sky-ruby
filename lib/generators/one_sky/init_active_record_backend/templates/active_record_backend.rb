be9417950812511725412ee244cd77e149853682
Matthew Rudy Jacobs <MatthewRudyJacobs@gmail.com>
2012-01-24 19:16:49 +0000

    the activerecord backend must come first

--- lib/generators/one_sky/init_active_record_backend/templates/active_record_backend.rb
+++ lib/generators/one_sky/init_active_record_backend/templates/active_record_backend.rb
@@ -7,7 +7,8 @@ I18n::Backend::ActiveRecord.send(:include, I18n::Backend::Flatten)
 # ensure we have migrated before we try and use the backend.
 if I18n::Backend::ActiveRecord::Translation.table_exists?
   # chain it into the existing backend
-  I18n.backend = I18n::Backend::Chain.new(I18n.backend, I18n::Backend::ActiveRecord.new)
+  # the Active Record backend must come first!
+  I18n.backend = I18n::Backend::Chain.new(I18n::Backend::ActiveRecord.new, I18n.backend)
 else
   warn "There is currently no translations table. You may need to migrate."
 end
