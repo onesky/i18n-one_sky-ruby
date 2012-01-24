ef7fa5f7d9533297a2554f66505bcbe435d709bd
Matthew Rudy Jacobs <MatthewRudyJacobs@gmail.com>
2012-01-24 19:16:29 +0000

    import the activerecord keys correctly

--- lib/i18n/one_sky/active_record_client.rb
+++ lib/i18n/one_sky/active_record_client.rb
@@ -7,8 +7,6 @@ module I18n
     # it encapsulates the logic of I18n::Backend::ActiveRecord
     class ActiveRecordClient < ClientBase
 
-      Translation = I18n::Backend::ActiveRecord::Translation
-
       def download
         puts "Downloading translations for I18n Active Record Backend:"
 
@@ -25,11 +23,8 @@ module I18n
           else
             yaml = platform.translation.download_yaml(locale_code)
             YAML.load(yaml).each do |code, translations|
-              puts "  locale: #{locale_code}, count: #{translations.length}"
-              translations.each do |key, value|
-                Translation.locale(i18n_locale_code).delete_all(["key=?", key])
-                Translation.create!(:locale => i18n_locale_code, :key => key.to_s, :value => value)
-              end
+              active_record_backend.store_translations(i18n_locale_code, translations)
+              puts "  locale: #{i18n_locale_code}, count: #{translation_scope(i18n_locale_code).count}"
             end
           end
         end
@@ -42,12 +37,28 @@ module I18n
       # Grab all the keys from the active record store.
       def all_phrases
         phrases = Hash.new
-        I18n::Backend::ActiveRecord::Translation.locale(I18n.default_locale).find_each() do |translation|
+        translation_scope(I18n.default_locale).find_each() do |translation|
           phrases[translation.key] = translation.value
         end
         phrases
       end
       memoize :all_phrases
+
+      protected
+
+      def active_record_backend
+        if I18n.backend.is_a?(I18n::Backend::Chain)
+          I18n.backend.backends.detect do |backend|
+            backend.is_a?(I18n::Backend::ActiveRecord)
+          end
+        else
+          I18n.backend
+        end
+      end
+
+      def translation_scope(locale)
+        I18n::Backend::ActiveRecord::Translation.locale(locale)
+      end
 
     end
   end
